c76dc4712a9a934c75f6da214b171cb8baaba4e4
JatoPriestly <njilahjato@gmail.com>
2024-11-26 16:32:48 +0100

    started work

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
-const path = require('path');
+const path = require('path'); 
 
 
 dotenv.config();
